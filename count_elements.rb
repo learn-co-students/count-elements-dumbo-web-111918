4a3f115af7d2a8711e388e08a41b6b4811996c18
github username <github email address>
2018-11-21 09:01:45 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
 def count_elements(array)
-  # code goes here
+  # code goes here\
+
+  count = {}
+
+  array.each do |string|
+    if count.keys.include?(string) == false
+      count[string] = 1
+    elsif count.keys.include?(string)
+      count[string] += 1
+    end
+  end
+  count
+
 end
- 
