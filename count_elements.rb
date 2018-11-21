67864c6d5745b6a0cf1500b1687faf1cd3ab259d
github username <github email address>
2018-11-20 19:56:11 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
-def count_elements(array)
-  # code goes here
+require 'pry'
+
+def count_elements(a)
+  counts = Hash.new 0
+
+a.each do |word|
+  counts[word] += 1
+  #  binding.pry
 end
- 
+return counts
+end
